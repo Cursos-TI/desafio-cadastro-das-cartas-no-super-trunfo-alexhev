869f48245b53dea283be567fa7a27cdc78a530e7
alexhev <alexsandrorenovaccio@gmail.com>
2025-08-15 06:28:25 +0000

    adicionei o if para população

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@ int main() {
     char estado_letra;
     char codigo[4];
     char cidade[50];
-    unsigned long int populacao;
+    unsigned long int populacao = 30;
     float area;
     double pib;
     int pturismo;
@@ -14,6 +14,7 @@ int main() {
     float denpoder, denpoder2;
     float respopulacao, resarea, respib, respturismo;
     float resultadopoder, resultadoDensidade;
+
     printf("Digite uma letra de A a Z \n");
     scanf("%c", &estado_letra);
      
@@ -23,8 +24,8 @@ int main() {
     printf("Digite a cidade: \n");
     scanf(" %[^\n]", cidade), getchar();
     
-    printf("População total: \n");
-    scanf("%lu", &populacao);
+    //printf("População total: \n");
+    //scanf("%lu", &populacao);
     
     printf("Digite a área total: \n");
     scanf("%f", &area);
@@ -47,7 +48,7 @@ int main() {
     char estado_letra2;
     char codigo2[4];
     char cidade2[50];
-    unsigned long int populacao2;
+    unsigned long int populacao2 = 10;
     float area2;
     double pib2;
     int pturismo2;
@@ -67,8 +68,8 @@ int main() {
     printf("Digite a cidade: \n");
     scanf(" %[^\n]", cidade2), getchar();
     
-    printf("População total: \n");
-    scanf("%lu", &populacao2);
+    //printf("População total: \n");
+    //scanf("%lu", &populacao2);
     
     printf("Digite a área total: \n");
     scanf("%f", &area2);
@@ -84,13 +85,14 @@ int main() {
     percapta2 = pib2 * 1000000000.0 / populacao2;
     denpoder2 = 1.0 / densip2;
     superpoder2 = pturismo2 + pib2 + area2 + populacao2 + denpoder2;
-    respopulacao = populacao > populacao2;
+    //respopulacao = populacao > populacao2;
     resarea = area > area2;
     respib = pib > pib2;
     respturismo = pturismo > pturismo2;
     resultadopoder = superpoder > superpoder2;
     resultadoDensidade = densip < densip2;
-    Exibindo parte 1:
+
+    //Exibindo parte 1:
     printf("Estado digitado: %c \n", estado_letra);
     printf("Código digitado: %c%s \n",estado_letra, codigo);
     printf("Cidade: %s \n", cidade);
@@ -102,11 +104,11 @@ int main() {
     printf("Per capta: %f \n", percapta);
 
 
-    Exibindo cartas 2:
+    //Exibindo cartas 2:
     printf("Estado digitado: %c \n", estado_letra2);
     printf("Código digitado: %c%s \n",estado_letra2, codigo2);
     printf("Cidade: %s \n", cidade2);
-    printf("População total: %lu \n", populacao2);
+    //printf("População total: %lu \n", populacao2);
     printf("Área total: %.2f \n", area2);
     printf("Pib total: %.2f \n", pib2);
     printf("Número total turistas: %d \n", pturismo2);
@@ -114,13 +116,16 @@ int main() {
     printf("Per capta: %f \n", percapta2 );
 
     //Resultado vencedor:
-    printf("População: Carta 1 venceu? %d\n Área: Carta 1 venceu? %d\n PIB: Carta 1 venceu?%d\n Pontots turísticos: Carta 1 venceu? %d\n Densidade populaciona: carta 1 venceu?%d\n Super poder: Carta 1 venceu? %d\n",
-        respopulacao, resarea, respib, respturismo, resultadoDensidade , resultadopoder);
+    /*printf("População: Carta 1 venceu? %d\n Área: Carta 1 venceu? %d\n PIB: Carta 1 venceu?%d\n Pontots turísticos: Carta 1 venceu? %d\n Densidade populaciona: carta 1 venceu?%d\n Super poder: Carta 1 venceu? %d\n",
+        respopulacao, resarea, respib, respturismo, resultadoDensidade , resultadopoder);*/
+    if ( populacao > populacao2){
+        printf("Carta 1 - população é: %lu \n, carta 1 Venceu!!!", populacao);
+    } else {
+        printf("Carta 2 - População é: %lu, carta 2 VENCEU!!! \n", populacao2);
+    }
+    
+    
+    
 
     return 0;
-
-
-
-
-
 }
